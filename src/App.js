c915a1efdd5a2f6f106bbbdc8f6df31fae368e43
Muhammad Talha <muhammadtalhamushtaq95@hotmail.com>
2020-11-27 02:35:36 +0500

    First react app with the live FAQs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Faq from './components/Faq/Faq';
+import {Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <Header />
+
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/faq" component={Faq} />
+
+        <Footer />
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
